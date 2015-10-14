33c9b495aeff12415b274727a72ed3370c8e214f
daniel-georgiev <daniko9505@gmail.com>
2015-10-14 22:16:38 +0300

    removed shitty requirements for passwords

--- PhotoContest/PhotoContest.Models/Image.cs
+++ PhotoContest/PhotoContest.Models/Image.cs
@@ -9,6 +9,8 @@ namespace PhotoContest.Models
     public class Image
     {
         public int Id { get; set; }
+        public string Title { get; set; }
+
         public string Url { get; set; }
     }
 }
