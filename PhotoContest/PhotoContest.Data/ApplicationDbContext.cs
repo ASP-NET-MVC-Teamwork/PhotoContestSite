9d010ce79aaac14255a307fdf3a9a7766e24bb21
daniel-georgiev <daniko9505@gmail.com>
2015-10-16 17:15:34 +0300

    AuditInfo implemented

--- PhotoContest/PhotoContest.Data/ApplicationDbContext.cs
+++ PhotoContest/PhotoContest.Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.EntityFramework;
 using PhotoContest.Models;
+using PhotoContest.Models.Common;
 
 namespace PhotoContest.Data
 {
@@ -18,6 +20,31 @@ namespace PhotoContest.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditInfo && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditInfo)entry.Entity;
+
+                if (entry.State == EntityState.Added)
+                {
+                    if (!entity.PreserveCreatedOn)
+                    {
+                        entity.CreatedOn = DateTime.Now;
+                    }
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
         }
     }
 }
