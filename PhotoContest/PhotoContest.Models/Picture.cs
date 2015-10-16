b4298c341c42de56ac5ebbd90f75edf1ce9e0706
daniel-georgiev <daniko9505@gmail.com>
2015-10-16 18:21:45 +0300

    register and login implemented with usernames

---
+++ PhotoContest/PhotoContest.Models/Picture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhotoContest.Models.Common;
+
+namespace PhotoContest.Models
+{
+    class Picture : AuditInfo, IDeletableEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; } 
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedOn { get; set; }
+    }
+}
