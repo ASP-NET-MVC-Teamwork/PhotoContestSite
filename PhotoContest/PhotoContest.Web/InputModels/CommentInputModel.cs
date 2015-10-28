637516096d0fe15ce65549ade5f9059cdfd45b0c
DanielJelev <subleden@abv.bg>
2015-10-28 16:55:43 +0200

    Add CommentController and Views

---
+++ PhotoContest/PhotoContest.Web/InputModels/CommentInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoContest.Web.InputModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CommentInputModel
+    {
+        [Required]
+        public string Text { get; set; }
+    }
+}
