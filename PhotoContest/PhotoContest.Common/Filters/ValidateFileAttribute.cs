7c10e2abd15b40a55ed42b925f02a87ccb2ddac4
Martin Kurshumov <mkurshumov1@gmail.com>
2015-11-04 20:42:47 +0200

    Photo validation for type, size and not selected photo.

---
+++ PhotoContest/PhotoContest.Common/Filters/ValidateFileAttribute.cs
@@ -0,0 +1,35 @@
+﻿namespace PhotoContest.Common.Filters
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
+
+    public class ValidateFileAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int MaxContentLength = 1024 * 1024 * 2; //2 MB
+            string[] allowedFileExtensions = { ".jpg", ".gif", ".png", ".jpeg" };
+
+            var file = value as HttpPostedFileBase;
+
+            if (file == null)
+            {
+                return false;
+            }
+            else if (!allowedFileExtensions.Contains(file.FileName.Substring(file.FileName.LastIndexOf('.'))))
+            {
+                ErrorMessage = "You can upload only photo of type: " + string.Join(", ", allowedFileExtensions);
+                return false;
+            }
+            else if (file.ContentLength > MaxContentLength)
+            {
+                ErrorMessage = "Your Photo is too large, maximum allowed size is: " + (MaxContentLength / 1024).ToString() + "MB";
+                return false;
+            }
+            else { 
+                return true;
+            }
+        }
+    }
+}
